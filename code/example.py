9a19920bf18440fefb2bd058bfc0741fa386ed64
Jakub Dokulil <kuba@Jakub-MacBook-Pro.local>
2020-08-24 22:06:47 +0200

    přidány návod, jak psát práci

---
+++ code/example.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def s(t):
+    return(1/2*9.81*t**2)
+
+t = np.linspace(0, 10, 1000)
+s = s(t)
+
+for i in s:
+    if s > 10:
+        print("docela daleko")
+    else:
+        print("nic moc")
